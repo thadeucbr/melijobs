dec7fc90f9947b6a9e90762b36ae1626df101b44
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-06 15:06:45 -0300

    atualizando importacao execute

--- schedules.js
+++ schedules.js
@@ -1,13 +1,13 @@
 const amazonJobs = require('./jobs/amazonJobs');
 const mercadoLivreJobs = require('./jobs/mercadoLivreJobs');
 const googleJobs = require('./jobs/googleJobs');
-const sendJobs = require('./jobs/sendJobs')
+const verifyJobs = require('./jobs/sendJobs')
 
 const execute = () => {
   setTimeout(() => amazonJobs(), 10000);
   setTimeout(() => mercadoLivreJobs(), 20000);
   setTimeout(() => googleJobs(), 30000)
-  setTimeout(() => sendJobs(), 40000)
+  setTimeout(() => verifyJobs(), 40000)
 }
 
 module.exports = execute;
