09c656d601754a743f778397474bdaba765e40b6
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-06 14:58:30 -0300

    adicionando modificacoes

--- schedules.js
+++ schedules.js
@@ -1,15 +1,14 @@
 const amazonJobs = require('./jobs/amazonJobs');
 const mercadoLivreJobs = require('./jobs/mercadoLivreJobs');
 const googleJobs = require('./jobs/googleJobs');
-const oracleJobs = require('./jobs/oracleJobs');
 const sendJobs = require('./jobs/sendJobs')
 
 const execute = () => {
-  oracleJobs()
   setTimeout(() => amazonJobs(), 10000);
   setTimeout(() => mercadoLivreJobs(), 20000);
   setTimeout(() => googleJobs, 30000)
-  setTimeout(() => sendJobs(), 40000)
+  // setTimeout(() => sendJobs(), 40000)
 }
 
+execute()
 module.exports = execute;
