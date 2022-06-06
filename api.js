06ddebef1dd43e0a18d48fcec4bf2654bdfe84c5
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-06 15:18:20 -0300

    atualizando importacao execute

--- api.js
+++ api.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const nodeCron = require('node-cron');
 const execute = require('./schedules');
+const verifyJobs = require('./jobs/sendJobs')
 
 const app = express();
 
-nodeCron.schedule('59 23 * * *', () => execute(), {
+nodeCron.schedule('58 23 * * *', () => execute(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
+
+nodeCron.schedule('59 23 * * *', () => verifyJobs(), {
   scheduled: true,
   timezone: 'America/Sao_Paulo',
 });
@@ -14,7 +20,17 @@ nodeCron.schedule('0 6 * * *', () => execute(), {
   timezone: 'America/Sao_Paulo',
 });
 
+nodeCron.schedule('1 6 * * *', () => verifyJobs(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
+
 nodeCron.schedule('0 12 * * *', () => execute(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
+
+nodeCron.schedule('1 12 * * *', () => verifyJobs(), {
   scheduled: true,
   timezone: 'America/Sao_Paulo',
 });
@@ -24,7 +40,13 @@ nodeCron.schedule('0 18 * * *', () => execute(), {
   timezone: 'America/Sao_Paulo',
 });
 
+nodeCron.schedule('1 18 * * *', () => verifyJobs(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
+
 execute()
+setTimeout(() => verifyJobs(), 50000)
 
 app.listen(3005, () => console.log('API rodando na porta 3005'));
 
--- schedules.js
+++ schedules.js
@@ -1,13 +1,11 @@
 const amazonJobs = require('./jobs/amazonJobs');
 const mercadoLivreJobs = require('./jobs/mercadoLivreJobs');
 const googleJobs = require('./jobs/googleJobs');
-const verifyJobs = require('./jobs/sendJobs')
 
 const execute = () => {
+  googleJobs()
   setTimeout(() => amazonJobs(), 10000);
   setTimeout(() => mercadoLivreJobs(), 20000);
-  setTimeout(() => googleJobs(), 30000)
-  setTimeout(() => verifyJobs(), 40000)
 }
 
 module.exports = execute;
