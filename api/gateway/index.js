cd00219687cad337aa53e4d5ab6a6cc5599b3bfd
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-12 19:05:56 -0300

    adicionando loterias

--- api/gateway/index.js
+++ api/gateway/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => res.status(200).send(`<h1>Gateway barbudo de pé</h1>`))
