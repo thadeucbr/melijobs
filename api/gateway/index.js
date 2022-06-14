35b69d56d73a255ab700d13c3e04587985f64594
Thadeucbr <thadeucbr@gmail.com>
2022-06-14 11:34:17 -0300

    a

--- api/gateway/index.js
+++ api/gateway/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express()
 
-app.use(cors('*'))
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => res.status(200).send(`<h1>Gateway barbudo de pé</h1>`))
