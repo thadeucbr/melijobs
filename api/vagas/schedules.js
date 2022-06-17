2be6163a1a7e75dc29cced56e59da878633e0a1f
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-17 13:41:26 -0300

    adicionando accenture

--- api/vagas/schedules.js
+++ api/vagas/schedules.js
@@ -4,14 +4,16 @@ const googleJobs = require('./jobs/googleJobs');
 const gupySaveJobs = require('./jobs/gupyJobs');
 const verifyJobs = require('./jobs/sendJobs');
 const nerdinSaveJobs = require('./jobs/nerdinJobs');
+const accentureSaveJobs = require('./jobs/accentureJobs');
 
 const execute = () => {
   googleJobs()
-  setTimeout(() => amazonJobs(), 10000);
-  setTimeout(() => mercadoLivreJobs(), 20000);
-  setTimeout(() => gupySaveJobs(), 30000);
-  setTimeout(() => nerdinSaveJobs(), 40000)
-  setTimeout(() => verifyJobs(), 60000)
+  setTimeout(() => amazonJobs(), 10*1000);
+  setTimeout(() => mercadoLivreJobs(), 20*1000);
+  setTimeout(() => gupySaveJobs(), 30*1000);
+  setTimeout(() => nerdinSaveJobs(), 40*1000)
+  setTimeout(() => accentureSaveJobs(), 60*1000)
+  setTimeout(() => verifyJobs(), 90*1000)
 }
 
 module.exports = execute;
