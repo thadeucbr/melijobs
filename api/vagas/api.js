f0260cb0d3bcaee157c9bccc8a4062e17e802ed9
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-17 14:10:37 -0300

    adicionando accenture

--- api/vagas/api.js
+++ api/vagas/api.js
@@ -3,34 +3,37 @@ const { schedule } = require('node-cron');
 const execute = require('./schedules');
 const sayHello = require('./utils/helloWhatsapp');
 const mongoConnection = require('./database/mongoose');
+const Whatsapp = require('./utils/whatsApp');
 
 const app = express();
 
-mongoConnection();
+// mongoConnection();
 
-schedule('58 23 * * *', () => execute(), {
-  scheduled: true,
-  timezone: 'America/Sao_Paulo',
-});
+// schedule('58 23 * * *', () => execute(), {
+//   scheduled: true,
+//   timezone: 'America/Sao_Paulo',
+// });
 
-schedule('0 6 * * *', () => execute(), {
-  scheduled: true,
-  timezone: 'America/Sao_Paulo',
-});
+// schedule('0 6 * * *', () => execute(), {
+//   scheduled: true,
+//   timezone: 'America/Sao_Paulo',
+// });
 
-schedule('0 12 * * *', () => execute(), {
-  scheduled: true,
-  timezone: 'America/Sao_Paulo',
-});
+// schedule('0 12 * * *', () => execute(), {
+//   scheduled: true,
+//   timezone: 'America/Sao_Paulo',
+// });
 
-schedule('0 18 * * *', () => execute(), {
-  scheduled: true,
-  timezone: 'America/Sao_Paulo',
-});
+// schedule('0 18 * * *', () => execute(), {
+//   scheduled: true,
+//   timezone: 'America/Sao_Paulo',
+// });
 
-execute()
-setTimeout(() => sayHello(), 120000)
+// execute()
+// setTimeout(() => sayHello(), 120000)
+const teste = new Whatsapp();
 
+teste.sendText('120363025308814807@g.us', '🤌')
 app.listen(3334, () => console.log('API rodando na porta 3334'));
 
 // https://github.com/tryber/sd-016-a-live-lectures/tree/lecture/27.3/praticalExemple/src
