c97448f682762904bf976e9da6e297aa3f45210b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-06-17 14:52:57 -0300

    adicionando accenture

--- api/vagas/api.js
+++ api/vagas/api.js
@@ -1,37 +1,34 @@
 const express = require('express');
 const { schedule } = require('node-cron');
 const execute = require('./schedules');
+
 const mongoConnection = require('./database/mongoose');
-const { default: Whatsapp } = require('./utils/whatsApp');
 
 const app = express();
 
-// mongoConnection();
+mongoConnection();
 
-// schedule('58 23 * * *', () => execute(), {
-//   scheduled: true,
-//   timezone: 'America/Sao_Paulo',
-// });
+schedule('58 23 * * *', () => execute(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
 
-// schedule('0 6 * * *', () => execute(), {
-//   scheduled: true,
-//   timezone: 'America/Sao_Paulo',
-// });
+schedule('0 6 * * *', () => execute(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
 
-// schedule('0 12 * * *', () => execute(), {
-//   scheduled: true,
-//   timezone: 'America/Sao_Paulo',
-// });
+schedule('0 12 * * *', () => execute(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
 
-// schedule('0 18 * * *', () => execute(), {
-//   scheduled: true,
-//   timezone: 'America/Sao_Paulo',
-// });
+schedule('0 18 * * *', () => execute(), {
+  scheduled: true,
+  timezone: 'America/Sao_Paulo',
+});
 
-// execute()
-
-const teste = new Whatsapp()
-teste.sendMessage('120363025308814807@g.us', '👌')
+execute()
 
 app.listen(3334, () => console.log('API rodando na porta 3334'));
 
--- api/vagas/utils/WhatsApp.js
+++ api/vagas/utils/WhatsApp.js
@@ -1,21 +1,21 @@
-const wa = require('@open-wa/wa-automate');
+// const wa = require('@open-wa/wa-automate');
 
-class Whatsapp {
-    _whatsConfig; 
-    constructor() {
-        this._whatsConfig = {
-            sessionId: "COVID_HELPER",
-            multiDevice: true,
-            authTimeout: 60,
-            logConsole: true,
-        };
-    }
+// class Whatsapp {
+//     _whatsConfig; 
+//     constructor() {
+//         this._whatsConfig = {
+//             sessionId: "COVID_HELPER",
+//             multiDevice: true,
+//             authTimeout: 60,
+//             logConsole: true,
+//         };
+//     }
 
-    async sendMessage(to, message) {
-        await wa.create(this._whatsConfig).then(async (client) => {
-            if(amigo) client.sendText(to, message);
-        });
-    }
-}
+//     async sendMessage(to, message) {
+//         await wa.create(this._whatsConfig).then(async (client) => {
+//             if(amigo) client.sendText(to, message);
+//         });
+//     }
+// }
 
-export default Whatsapp;
+// export default Whatsapp;
